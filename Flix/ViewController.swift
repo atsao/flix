8f9a67f5de1f37864e23fd1eb28f56e8d726377a
Andrew Tsao <andrew.tsao8@gmail.com>
2016-10-14 21:21:21 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Andrew Tsao on 10/14/16.
+//  Copyright © 2016 Andrew Tsao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
