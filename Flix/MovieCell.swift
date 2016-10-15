39be93e30f7e7d1d854bce7924aff11ab9dd255c
Andrew Tsao <andrew.tsao8@gmail.com>
2016-10-15 13:13:17 -0700

    Initializes repo, adds basic functionality

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Andrew Tsao on 10/15/16.
+//  Copyright © 2016 Andrew Tsao. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix/MoviesViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MoviesViewController.swift
+//  Flix
+//
+//  Created by Andrew Tsao on 10/14/16.
+//  Copyright © 2016 Andrew Tsao. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var MovieTableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MovieTableView.dataSource = self
+        MovieTableView.delegate = self
+        
+        let api_key = "1963f8d3c739cf3c9117d9ef475f6935"
+        let api_now_playing = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(api_key)")
+        let request = NSURLRequest(url: api_now_playing! as URL)
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task : URLSessionDataTask = session.dataTask(with: request as URLRequest, completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    self.movies = responseDictionary["results"] as? [NSDictionary]
+                    self.MovieTableView.reloadData()
+                }
+            }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = MovieTableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
+        cell.titleLabel.text = "\(title)"
+        cell.overviewLabel.text = "\(overview)"
+
+        
+        return cell
+    }
+
+}
